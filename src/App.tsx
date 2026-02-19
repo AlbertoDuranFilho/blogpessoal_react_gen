2b9f752398d2e5eb14999e376d75e2cc59e926f5
Alberto Duran <albertoduranfilho@gmail.com>
2026-02-19 14:59:09 -0300

    :sparkles: feat: Adicionando Cadastro dos temas.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import FormTema from "./components/tema/formtema/FormTema";
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
             </Routes>
           </div>
           <Footer />
