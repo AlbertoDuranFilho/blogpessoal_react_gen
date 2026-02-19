5ca678cb5df80015eee12cbe63a086248aef7262
Alberto Duran <albertoduranfilho@gmail.com>
2026-02-19 14:48:15 -0300

    :sparkles: feat: Adicionando funcionalidade de tema (card, lista e rota).

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import ListaTemas from "./components/tema/listatemas/ListaTema";
 import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
@@ -15,9 +16,10 @@ function App() {
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
